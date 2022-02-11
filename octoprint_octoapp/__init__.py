a87dbe8e244d0f8ed996e8640ce88adc11ed9822
Christian Würthner <c.wurthner@gmail.com>
2022-02-11 15:43:01 +0100

    Add support for MMU2 Filament Select

--- octoprint_octoapp/__init__.py
+++ octoprint_octoapp/__init__.py
@@ -8,6 +8,7 @@ import logging
 from pkgutil import get_data
 import threading
 import time
+from typing import Dict
 import uuid
 import os
 
@@ -47,10 +48,12 @@ class OctoAppPlugin(
         self.last_progress = None
         self.last_print_name = None
         self.firmware_info = {}
-        self.last_m117_message = None
+        self.plugin_state = {}
+        self.last_send_plugin_state = {}
 
-    def get_settings_defaults(self):
-        return dict(encryptionKey=None)
+    #
+    # EVENTS
+    #
 
     def on_after_startup(self):
         self._logger.info("OctoApp started, updating config")
@@ -60,148 +63,11 @@ class OctoAppPlugin(
         self.get_or_create_encryption_key()
         self.update_data_structure()
 
-    def get_template_configs(self):
-        return [dict(type="settings", custom_bindings=True)]
-
-    def get_api_commands(self):
-        return dict(
-            registerForNotifications=[],
-            getPrinterFirmware=[],
-        )
-
-    def on_print_progress(self, storage, path, progress):
-        self.last_progress = progress
-        self.last_time_left = self._printer.get_current_data()["progress"][
-            "printTimeLeft"
-        ]
-
-        # send update, but don't send for 100%
-        # we send updated in "modulus" interval as well as for the first and last "modulus" percent
-        config = self.get_config()
-        modulus = config["updatePercentModulus"]
-        highPrecisionStart = config["highPrecisionRangeStart"]
-        highPrecisionEnd = config["highPrecisionRangeEnd"]
-        if progress < 100 and (
-            (progress % modulus) == 0
-            or progress <= highPrecisionStart
-            or progress >= (100 - highPrecisionEnd)
-        ):
-            self.send_notification(
-                dict(
-                    type="printing",
-                    fileName=self.last_print_name,
-                    progress=self.last_progress,
-                    timeLeft=self.last_time_left,
-                ),
-                False,
-            )
-
-    def on_event(self, event, payload):
-        self._logger.debug("Recevied event %s" % event)
-        if event == "PrintStarted":
-            self.last_print_name = payload["name"]
-            self.last_progress_notification_at = 0
-            self.last_m117_message = None
-            self.send_m117_plugin_message()
-
-        if event == "PrintDone":
-            self.last_progress = None
-            self.last_print_name = None
-            self.last_time_left = None
-            self.send_notification(
-                dict(type="completed", fileName=payload["name"]), True)
-            self.last_m117_message = None
-            self.send_m117_plugin_message()
-
-        elif event == "PrintFailed" or event == "PrintCancelled":
-            self.last_progress = None
-            self.last_print_name = None
-            self.send_notification(
-                dict(type="idle", fileName=payload["name"]), False)
-            self.last_m117_message = None
-            self.send_m117_plugin_message()
-
-        elif event == "FilamentChange" and self.last_progress is not None:
-            self.send_notification(
-                dict(
-                    type="filament_required",
-                    fileName=self.last_print_name,
-                    progress=self.last_progress,
-                    timeLeft=self.last_time_left,
-                ),
-                True,
-            )
-
-        elif event == Events.CLIENT_OPENED:
-            self.send_m117_plugin_message()
-            self.send_settings_plugin_message(self.get_apps())
-
-        elif event == "PrintPaused":
-            self.send_notification(
-                dict(
-                    type="paused",
-                    fileName=payload["name"],
-                    progress=self.last_progress,
-                    timeLeft=self.last_time_left,
-                ),
-                False,
-            )
-
-    def send_notification(self, data, highPriority):
-        t = threading.Thread(target=self.do_send_notification, args=[
-                             data, highPriority])
-        t.daemon = True
-        t.start()
-
-    def do_send_notification(self, data, highPriority):
-        try:
-            config = self.get_config()
-
-            # encrypt message and build request body
-            data["serverTime"] = int(time.time())
-            cipher = AESCipher(self.get_or_create_encryption_key())
-            data = cipher.encrypt(json.dumps(data))
-            apps = self.get_apps()
-            if not apps:
-                self._logger.debug("No apps registered, skipping notification")
-                return
-
-            body = dict(targets=apps, highPriority=highPriority, data=data)
-            self._logger.debug("Sending notification %s" % body)
-
-            # make request and check 200
-            r = requests.post(config["sendNotificationUrl"],
-                              timeout=float(15), json=body)
-            if r.status_code != requests.codes.ok:
-                raise Exception("Unexpected response code %d" % r.status_code)
-
-            # delete invalid tokens
-            apps = self.get_apps()
-            self._logger.debug("Before updating apps %s" % apps)
-            for fcmToken in r.json()["invalidTokens"]:
-                apps = [app for app in apps if app["fcmToken"] != fcmToken]
-            self.set_apps(apps)
-            self._settings.save()
-            self._logger.debug("Updated apps %s" % apps)
-        except Exception as e:
-            self._logger.debug("Failed to send notification %s" % e)
-
     def on_firmware_info_received(
         self, comm_instance, firmware_name, firmware_data, *args, **kwargs
     ):
         self._logger.debug("Recevied firmware info")
         self.firmware_info = firmware_data
-
-    def processGcode(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-        if gcode == "M117":
-            self.last_m117_message = cmd.split(' ', 1)[1]
-            self._logger.debug("M117 message changed: %s" %
-                               self.last_m117_message)
-            self.send_m117_plugin_message()
-
-    def send_m117_plugin_message(self):
-        self._plugin_manager.send_plugin_message(
-            self._identifier, {"m117": self.last_m117_message})
 
     def on_api_command(self, command, data):
         self._logger.debug("Recevied command %s" % command)
@@ -246,6 +112,172 @@ class OctoAppPlugin(
 
         return flask.jsonify(dict())
 
+    def on_emit_websocket_message(self, user, message, type, data):
+        if type == "plugin" and data.get("plugin") == "mmu2filamentselect" and isinstance(data.get("data"), dict):
+            action = data.get("data").get("action")
+
+            if action == "show":
+                # If not currently active, send notification as we switched state
+                if  self.plugin_state.get("mmuSelectionActive") is not True:
+                    self.send_notification(
+                        dict(
+                            type="mmu_filament_selection",
+                            fileName=self.last_print_name,
+                            progress=self.last_progress,
+                            timeLeft=self.last_time_left,
+                        ),
+                        True,
+                    )
+
+                self.plugin_state["mmuSelectionActive"] = True
+                self.send_plugin_state_message()
+
+            elif action == "close":
+                self.plugin_state["mmuSelectionActive"] = False
+                self.send_plugin_state_message()
+
+        # Always return true! Returning false will prevent the message from being send
+        return True
+
+    def on_print_progress(self, storage, path, progress):
+        self.last_progress = progress
+        self.last_time_left = self._printer.get_current_data()["progress"][
+            "printTimeLeft"
+        ]
+
+        # send update, but don't send for 100%
+        # we send updated in "modulus" interval as well as for the first and last "modulus" percent
+        config = self.get_config()
+        modulus = config["updatePercentModulus"]
+        highPrecisionStart = config["highPrecisionRangeStart"]
+        highPrecisionEnd = config["highPrecisionRangeEnd"]
+        if progress < 100 and (
+            (progress % modulus) == 0
+            or progress <= highPrecisionStart
+            or progress >= (100 - highPrecisionEnd)
+        ):
+            self.send_notification(
+                dict(
+                    type="printing",
+                    fileName=self.last_print_name,
+                    progress=self.last_progress,
+                    timeLeft=self.last_time_left,
+                ),
+                False,
+            )
+
+    def on_event(self, event, payload):
+        self._logger.debug("Recevied event %s" % event)
+        if event == Events.PRINT_STARTED:
+            self.last_print_name = payload["name"]
+            self.last_progress_notification_at = 0
+            self.plugin_state["m117"] = None
+            self.send_plugin_state_message()
+
+        if event == Events.PRINT_DONE:
+            self.last_progress = None
+            self.last_print_name = None
+            self.last_time_left = None
+            self.send_notification(
+                dict(type="completed", fileName=payload["name"]), True)
+            self.plugin_state["m117"] = None
+            self.send_plugin_state_message()
+
+        elif event == Events.PRINT_FAILED or event == Events.PRINT_CANCELLED:
+            self.last_progress = None
+            self.last_print_name = None
+            self.send_notification(
+                dict(type="idle", fileName=payload["name"]), False)
+            self.plugin_state["m117"] = None
+            self.send_plugin_state_message()
+
+        elif event == Events.FILAMENT_CHANGE and self.last_progress is not None:
+            self.send_notification(
+                dict(
+                    type="filament_required",
+                    fileName=self.last_print_name,
+                    progress=self.last_progress,
+                    timeLeft=self.last_time_left,
+                ),
+                True,
+            )
+
+        elif event == Events.CLIENT_OPENED:
+            self.send_plugin_state_message(forced=True)
+            self.send_settings_plugin_message(self.get_apps())
+
+        elif event == Events.PRINT_PAUSED:
+            self.send_notification(
+                dict(
+                    type="paused",
+                    fileName=payload["name"],
+                    progress=self.last_progress,
+                    timeLeft=self.last_time_left,
+                ),
+                False,
+            )
+
+    def on_gcode_send(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+        if gcode == "M117":
+            message = cmd.split(' ', 1)[1]
+            self.plugin_state["m117"] = message
+            self._logger.debug("M117 message changed: %s" % message)
+            self.send_plugin_state_message()
+
+    #
+    # NOTIFICATIONS
+    #
+
+    def send_notification(self, data, highPriority):
+        t = threading.Thread(target=self.do_send_notification, args=[
+                             data, highPriority])
+        t.daemon = True
+        t.start()
+
+    def do_send_notification(self, data, highPriority):
+        try:
+            config = self.get_config()
+
+            # encrypt message and build request body
+            data["serverTime"] = int(time.time())
+            cipher = AESCipher(self.get_or_create_encryption_key())
+            data = cipher.encrypt(json.dumps(data))
+            apps = self.get_apps()
+            if not apps:
+                self._logger.debug("No apps registered, skipping notification")
+                return
+
+            body = dict(targets=apps, highPriority=highPriority, data=data)
+            self._logger.debug("Sending notification %s" % body)
+
+            # make request and check 200
+            r = requests.post(config["sendNotificationUrl"],
+                              timeout=float(15), json=body)
+            if r.status_code != requests.codes.ok:
+                raise Exception("Unexpected response code %d" % r.status_code)
+
+            # delete invalid tokens
+            apps = self.get_apps()
+            self._logger.debug("Before updating apps %s" % apps)
+            for fcmToken in r.json()["invalidTokens"]:
+                apps = [app for app in apps if app["fcmToken"] != fcmToken]
+            self.set_apps(apps)
+            self._settings.save()
+            self._logger.debug("Updated apps %s" % apps)
+        except Exception as e:
+            self._logger.debug("Failed to send notification %s" % e)
+
+    def send_plugin_state_message(self, forced=False):
+        # Only send if we are forced to update or the state actually changed
+        if forced or self.last_send_plugin_state != self.plugin_state:
+            self.last_send_plugin_state = self.plugin_state.copy()
+            self._plugin_manager.send_plugin_message(
+                self._identifier, self.plugin_state)
+
+    #
+    # CONFIG
+    #
+
     def get_config(self):
         t = threading.Thread(target=self.do_update_config)
         t.daemon = True
@@ -278,18 +310,9 @@ class OctoAppPlugin(
             self._logger.info("OctoApp loaded config: %s" % self.cached_config)
             self.cached_config_at = cache_config_max_age + 300
 
-    def get_update_information(self):
-        return dict(
-            octoapp=dict(
-                displayName="OctoApp",
-                displayVersion=self._plugin_version,
-                type="github_release",
-                current=self._plugin_version,
-                user="crysxd",
-                repo="OctoApp-Plugin",
-                pip="https://github.com/crysxd/OctoApp-Plugin/archive/{target}.zip",
-            )
-        )
+    #
+    # APPS
+    #
 
     def update_data_structure(self):
         if not os.path.isfile(self.data_file):
@@ -302,7 +325,10 @@ class OctoAppPlugin(
 
     def get_apps(self):
         with open(self.data_file, 'r') as file:
-            return json.load(file)
+            apps = json.load(file)
+        if apps is None:
+            apps = []
+        return apps
 
     def set_apps(self, apps):
         with open(self.data_file, 'w') as outfile:
@@ -314,6 +340,35 @@ class OctoAppPlugin(
             displayName=x["displayName"], lastSeenAt=x["lastSeenAt"]), apps))
         self._plugin_manager.send_plugin_message(
             "%s.settings" % self._identifier, {"apps": mapped_apps})
+
+    #
+    # MISC
+    #
+
+    def get_settings_defaults(self):
+        return dict(encryptionKey=None)
+
+    def get_template_configs(self):
+        return [dict(type="settings", custom_bindings=True)]
+
+    def get_api_commands(self):
+        return dict(
+            registerForNotifications=[],
+            getPrinterFirmware=[],
+        )
+
+    def get_update_information(self):
+        return dict(
+            octoapp=dict(
+                displayName="OctoApp",
+                displayVersion=self._plugin_version,
+                type="github_release",
+                current=self._plugin_version,
+                user="crysxd",
+                repo="OctoApp-Plugin",
+                pip="https://github.com/crysxd/OctoApp-Plugin/archive/{target}.zip",
+            )
+        )
 
     def get_or_create_encryption_key(self):
         key = self._settings.get(["encryptionKey"])
@@ -343,8 +398,6 @@ class OctoAppPlugin(
                  default_groups=[ADMIN_GROUP, USER_GROUP, READONLY_GROUP])
         ]
 
-    # ~~ AssetPlugin mixin
-
     def get_assets(self):
         return dict(
             js=[
@@ -359,8 +412,9 @@ __plugin_implementation__ = OctoAppPlugin()
 __plugin_hooks__ = {
     "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
     "octoprint.comm.protocol.firmware.info": __plugin_implementation__.on_firmware_info_received,
-    "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.processGcode,
+    "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.on_gcode_send,
     "octoprint.access.permissions": __plugin_implementation__.get_additional_permissions,
+    "octoprint.server.sockjs.emit": __plugin_implementation__.on_emit_websocket_message,
 }
 
 
