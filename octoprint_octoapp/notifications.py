489f9d76867035bd9d534266ea6b1944282a7641
Christian Würthner <c.wurthner@gmail.com>
2023-04-29 04:27:54 +0200

    Add support for pause action

--- octoprint_octoapp/notifications.py
+++ octoprint_octoapp/notifications.py
@@ -188,7 +188,7 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
             else:
                 self._logger.debug("NOTIFICATION | Skipping beep, only %s seconds left and %s percent" % (time_left, progress))    
     
-        elif gcode == "M601" or cmd == "@pause":
+        elif gcode == "M601" or cmd == "@pause" or cmd == "// action:pause" or cmd == "//action:pause":
             self.update_print_state()
             self.send_notification(event=self.EVENT_PAUSED_GCODE)
 			
