1c2e0f09c9fe7e2b7497d19cb1a2f9e22ec117e8
Christian Würthner <c.wurthner@gmail.com>
2023-08-20 07:20:53 +0200

    Fix issue with Dashbaord plugin

--- octoprint_octoapp/printer_message.py
+++ octoprint_octoapp/printer_message.py
@@ -14,7 +14,8 @@ class OctoAppPrinterMessageSubPlugin(OctoAppSubPlugin):
         if gcode == "M117":
             message = cmd.split(' ', 1)[1]
             self.parent.plugin_state["m117"] = message
-            self._logger.debug("MESSAGE      | M117 message changed: %s" % message)
+            # This causes blobs in the print when combined with the Dasboard plugin
+            #self._logger.debug("MESSAGE      | M117 message changed: %s" % message)
             self.parent.send_plugin_state_message()
             return
 
