6c05adaffbc7dbc256d0229ed8ac90497a3652bb
Christian Würthner <5859228+crysxd@users.noreply.github.com>
2023-11-28 10:26:49 +0100

    Add PrintTimeGenius support

--- octoprint_octoapp/notifications.py
+++ octoprint_octoapp/notifications.py
@@ -8,6 +8,10 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
     def __init__(self, parent, notification_handler: NotificationsHandler):
         super().__init__(parent)
         self.NotificationHandler = notification_handler
+        self._hasPrintTimeGenius = self.parent._plugin_manager.get_plugin("PrintTimeGenius") is not None
+        self.Progress = 0
+        self.GcodeSentCount = 0
+
 
     def _getPrinterName(self):
         name = self.parent._settings.global_get(
@@ -19,14 +23,19 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
 
         return name
 
+
     def OnAfterStartup(self):
         self.NotificationHandler.NotificationSender.PrinterName = self._getPrinterName()
+        Sentry.Info("NOTIFICATION",  "Has PrintTimeGenius: %s" % self._hasPrintTimeGenius)
+
 
     def OnPrintProgress(self, storage, path, progress):
-        if self.NotificationHandler is not None:
-            self.NotificationHandler.OnPrintProgress(progress, None)
+        self._updateProgressAndSendIfChanged()
+
 
     def OnEvent(self, event, payload):       
+        self._updateProgressAndSendIfChanged()
+
         # Only check the event after the notification handler has been created.
         # Specifically here, we have seen the Error event be fired before `on_startup` is fired,
         # and thus the handler isn't created.
@@ -40,6 +49,8 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
         # Listen for the rest of these events for notifications.
         # OctoPrint Events
         if event == "PrintStarted":
+            self.Progress = 0
+            self.GcodeSentCount = 0
             fileName = self.GetDictStringOrEmpty(payload, "name")
             # Gather some stats from other places, if they exist.
             currentData = self.parent._printer.get_current_data()
@@ -49,6 +60,7 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
             totalFilamentUsageMm = 0
             if self._exists(currentData, "job") and self._exists(currentData["job"], "filament") and self._exists(currentData["job"]["filament"], "tool0") and self._exists(currentData["job"]["filament"]["tool0"], "length"):
                 totalFilamentUsageMm = int(currentData["job"]["filament"]["tool0"]["length"])
+            self._updateProgressAndSendIfChanged()
             self.NotificationHandler.OnStarted(fileName, fileSizeKBytes, totalFilamentUsageMm)
         elif event == "PrintFailed" or event == "PrintCancelled":
             fileName = self.GetDictStringOrEmpty(payload, "name")
@@ -83,8 +95,16 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
             # Name might have changed
             self.NotificationHandler.NotificationSender.PrinterName = self._getPrinterName()
 
+
     def OnGcodeSent(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
         # Blocking will block the printer commands from being handled so we can't block here!
+
+        # Check for progress updates every 250 gcode commands. If we have PrintTimeGenius the
+        # progress shown to the user might update outside of OctoPrint's progress updates, so 
+        # to keep up we regularly check during the print
+        self.GcodeSentCount = self.GcodeSentCount + 1
+        if self.GcodeSentCount % 250 == 0 and self._hasPrintTimeGenius:
+            self._updateProgressAndSendIfChanged()
 
         # M600 is a filament change command.
         # https://marlinfw.org/docs/gcode/M600.html
@@ -117,6 +137,7 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
             except Exception as e:
                 Sentry.Warn("Failed to parse gcode %s, error %s" % (cmd, str(e)))
     
+
     def OnGcodeReceived(self, comm_instance, line, *args, **kwargs):
         # Blocking will block the printer commands from being handled so we can't block here!
 
@@ -152,12 +173,15 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
         # We must return line the line won't make it to OctoPrint!
         return line
     
+
     # A dict helper
     def _exists(self, dictObj:dict, key:str) -> bool:
         return key in dictObj and dictObj[key] is not None
 
+
     def GetDictStringOrEmpty(self, d, key):
         return str(d.get(key, ""))
+
 
     # Gets the current setting or the default value.
     def GetBoolFromSettings(self, name, default):
@@ -166,10 +190,33 @@ class OctoAppNotificationsSubPlugin(OctoAppSubPlugin):
             return default
         return value is True
 
+
     # Gets the current setting or the default value.
     def GetFromSettings(self, name, default):
         value = self._settings.get([name])
         if value is None:
             return default
         return value
+    
+
+    # Depending on if we have PrintTimeGenius, use OctoPrints progress or emulate the PrintTimeGenius calculation 
+    # (based on the printTimeLeft which is modified by PrintTimeGenius)
+    def _updateProgressAndSendIfChanged(self):
+        progress = self.parent._printer.get_current_data().get("progress", {})
+        printTimeLeft = progress["printTimeLeft"]
+        printTime = progress["printTime"]
+        completion = progress["completion"]
+        lastProgress = self.Progress
+
+        if completion is None or printTime is None or printTimeLeft is None:
+            return
+
+        if self._hasPrintTimeGenius and printTime is not None and printTimeLeft is not None:
+            self.Progress = int((printTime / float(printTime + printTimeLeft)) * 100.0)
+        else:
+            self.Progress = int(completion)
+
+        if self.Progress != lastProgress and self.NotificationHandler is not None:
+            Sentry.Debug("NOTIFICATION", "Progress change: %s -> %s" % (lastProgress, self.Progress))
+            self.NotificationHandler.OnPrintProgress(progress, None)
     
