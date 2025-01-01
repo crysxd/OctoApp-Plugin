fcc35182bf4a0a5a2ac2a8c7ac3e43277518b3ec
Christian Würthner <5859228+crysxd@users.noreply.github.com>
2025-01-01 10:08:06 +0100

    Potential fix for Joe's K1

--- install.sh
+++ install.sh
@@ -188,7 +188,7 @@ ensure_py_venv()
         # The K1 requires we setup the virtualenv like this.
         if [[ -f /opt/bin/python3 ]]
         then
-            virtualenv -p /opt/bin/python3 --system-site-packages "${OCTOAPP_ENV}"
+            /opt/bin/virtualenv -p /opt/bin/python3 --system-site-packages "${OCTOAPP_ENV}"
         else
             python3 /usr/lib/python3.8/site-packages/virtualenv.py -p /usr/bin/python3 --system-site-packages "${OCTOAPP_ENV}"
         fi
