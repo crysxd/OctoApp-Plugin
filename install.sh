629c4a407198488c1b275eb636d813183ada99bc
knoe103003 <62354449+knoe103003@users.noreply.github.com>
2025-03-30 03:05:22 -0400

    Correct typo in install.sh (#111)
    Change "optinal" to "optional" on line 289.

--- install.sh
+++ install.sh
@@ -286,7 +286,7 @@ install_or_update_python_env()
 
     if [[ $IS_K1_OS -ne 1 ]]
     then
-        log_info "Attempting to install optinal python libs..."
+        log_info "Attempting to install optional python libs..."
          "${OCTOAPP_ENV}"/bin/pip3 install -q -r "${OCTOAPP_REPO_DIR}"/requirements_try.txt || PIP_FAILED="true"
         if [ "$PIP_FAILED" = "true" ]
         then 
