66e2337db0e2b89d3ae8c03f4dfe7dcf8c0a748a
Bryan Merrill <brymerr921@gmail.com>
2025-08-21 01:33:46 -0700

    Initial docker test

---
+++ docker_octoapp/__main__.py
@@ -0,0 +1,206 @@
+import os
+import sys
+import json
+import time
+import signal
+import base64
+import logging
+import traceback
+import subprocess
+
+from enum import Enum
+from typing import Any, Optional
+
+#
+# This docker host is the entry point for the docker container.
+# Unlike the other host, this host doesn't run the service, it invokes the bambu or companion host.
+#
+
+from linux_host.startup import Startup
+from linux_host.config import Config
+
+from .BambuBootstrap import BambuBootstrap
+from .ElegooBootstrap import ElegooBootstrap
+from .KlipperBootstrap import KlipperBootstrap
+
+
+# Possible modes for the companion.
+class CompanionMode(Enum):
+    BambuConnect  = 1
+    ElegooConnect = 2
+    Klipper   = 3
+
+    # Makes to str() cast not to include the class name.
+    def __str__(self):
+        return self.name
+
+
+# pylint: disable=logging-fstring-interpolation
+
+if __name__ == '__main__':
+
+    # Setup a basic logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    std = logging.StreamHandler(sys.stdout)
+    std.setFormatter(formatter)
+    logger.addHandler(std)
+
+    logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    logger.info("Starting Docker OctoApp Bootstrap")
+    logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+    # This is a helper class, to keep the startup logic common.
+    s = Startup()
+
+    #
+    # Helper functions
+    #
+    def LogException(msg:str, e:Exception) -> None:
+        tb = traceback.format_exc()
+        exceptionClassType = "unknown_type"
+        if e is not None:
+            exceptionClassType = e.__class__.__name__
+        logger.error(f"{msg}; {str(exceptionClassType)} Exception: {str(e)}; {str(tb)}")
+
+    def EnsureIsPath(path: Optional[str]) -> str:
+        logger.info(f"Ensuring path exists: {path}")
+        if path is None or not os.path.exists(path):
+            raise Exception(f"Path does not exist: {path}")
+        return path
+
+    def CreateDirIfNotExists(path: str) -> None:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    try:
+        # First, read the required env vars that are set in the dockerfile.
+        virtualEnvPath = EnsureIsPath(os.environ.get("VENV_DIR", None))
+        repoRootPath = EnsureIsPath(os.environ.get("REPO_DIR", None))
+
+        # Handle this path differently, since the user has to set it up correctly, we will give a better error if it doesn't exist.
+        dataPath = os.environ.get("DATA_DIR", None)
+        if dataPath is None:
+            raise Exception("DATA_DIR environment variable is not set.")
+        if not os.path.exists(dataPath):
+            logger.error("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            logger.error(" The required /data/ directory doesn't exist which means there's no docker volume mounted for it.")
+            logger.error(" Add this:Use -v ./local/path:/data .")
+            logger.error("   Docker Command Line: -v ./local/path:/data")
+            logger.error("   Docker Compose: volumes: - ./local/path:/data")
+            logger.error(" See this guide for more details:")
+            logger.error(" https://github.com/QuinnDamerell/OctoPrint-OctoEverywhere/blob/master/docker-readme.md")
+            logger.error("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            # Sleep for a bit, so if we are restarted we don't do it instantly.
+            time.sleep(3)
+            sys.exit(1)
+
+        # For Bambu Connect, the config sits int the data dir.
+        configPath = dataPath
+
+        # Create the config object, which will read an existing config or make a new one.
+        # If this is the first run, there will be no config file, so we need to create one.
+        logger.info(f"Init config object: {configPath}")
+        config = Config(configPath)
+
+        #
+        #
+        # Step 1: Figure out the Companion mode.
+        #
+        #
+
+        # Default to Bambu Connect, since it existed before we supported different modes.
+        mode:CompanionMode = CompanionMode.BambuConnect
+        modeStr = os.environ.get("COMPANION_MODE", None)
+        if modeStr is not None:
+            modeStr = modeStr.lower()
+            if modeStr == "bambu":
+                mode = CompanionMode.BambuConnect
+            elif modeStr == "elegoo":
+                mode = CompanionMode.ElegooConnect
+            elif modeStr == "klipper":
+                mode = CompanionMode.Klipper
+            logger.info(f"Companion mode: {mode}")
+        else:
+            logger.info("No companion mode set, defaulting to Bambu Connect.")
+
+        #
+        #
+        # Step 1: Ensure all required vars are set.
+        #
+        #
+        if mode == CompanionMode.BambuConnect:
+            BambuBootstrap.Bootstrap(logger, config)
+        elif mode == CompanionMode.ElegooConnect:
+            ElegooBootstrap.Bootstrap(logger, config)
+        elif mode == CompanionMode.Klipper:
+            KlipperBootstrap.Bootstrap(logger, config)
+        else:
+            raise Exception(f"Invalid companion mode: {mode}")
+
+        # Create the rest of the required dirs based in the data dir, since it's persistent.
+        localStoragePath = os.path.join(dataPath, "octoapp-store")
+        CreateDirIfNotExists(localStoragePath)
+        logDirPath = os.path.join(dataPath, "logs")
+        CreateDirIfNotExists(logDirPath)
+
+        # If there's a local web api port, set it in the config.
+        localApiPort = os.environ.get("LOCAL_API_PORT", None)
+        if localApiPort is not None:
+            try:
+                config.SetInt(Config.GeneralSection, Config.GeneralLocalWebApiPort, int(localApiPort), False)
+            except Exception as e:
+                logger.error(f"Failed to set local API port to value `{localApiPort}`, error: {e}")
+                logger.error("Make sure the value is a valid integer, or omit it.")
+                sys.exit(1)
+
+        # Build the launch string
+        launchConfig = {
+            "ServiceName" : "octoapp", # Since there's only once service, use the default name.
+            "CompanionInstanceIdStr" : "1",   # Since there's only once service, use the default service id.
+            "VirtualEnvPath" : virtualEnvPath,
+            "RepoRootFolder" : repoRootPath,
+            "LocalFileStoragePath" : localStoragePath,
+            "LogFolder" : logDirPath,
+            "ConfigFolder" : configPath,
+            "IsCompanion" : True,
+            "IsDockerContainer" : True,
+        }
+
+        # Convert the launch string into what's expected.
+        launchConfigStr = json.dumps(launchConfig)
+        logger.info(f"Launch config: {launchConfigStr}")
+        base64EncodedLaunchConfig =  base64.urlsafe_b64encode(bytes(launchConfigStr, "utf-8")).decode("utf-8")
+
+        # Setup a ctl-c handler, so the docker container can be closed easily.
+        def signal_handler(sig:Any, frame:Any) -> None:
+            logger.info("OctoApp Connect Docker - container stop requested")
+            sys.exit(0)
+        signal.signal(signal.SIGINT, signal_handler)
+
+        # Get the correct package to launch.
+        pyPackage = "bambu_octoapp"
+        if mode == CompanionMode.ElegooConnect:
+            pyPackage = "elegoo_octoapp"
+        elif mode == CompanionMode.Klipper:
+            pyPackage = "moonraker_octoapp"
+
+        # Instead of running the plugin in our process, we decided to launch a different process so it's clean and runs
+        # just like the plugin normally runs.
+        pythonPath = os.path.join(virtualEnvPath, os.path.join("bin", "python3"))
+        logger.info(f"Launch PY path: {pythonPath}")
+        result:subprocess.CompletedProcess = subprocess.run([pythonPath, "-m", pyPackage, base64EncodedLaunchConfig], check=False)
+
+        # Normally the process shouldn't exit unless it hits a bad error.
+        if result.returncode == 0:
+            logger.info(f"OctoApp Connect Docker - plugin exited. Result: {result.returncode}")
+        else:
+            logger.error(f"OctoApp Connect Docker - plugin exited with an error. Result: {result.returncode}")
+
+    except Exception as e:
+        LogException("Exception while bootstrapping up OctoApp Connect.", e)
+
+    # Sleep for a bit, so if we are restarted we don't do it instantly.
+    time.sleep(3)
+    sys.exit(1)
